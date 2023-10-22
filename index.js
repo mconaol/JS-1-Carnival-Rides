9e51725100cf0b0f2261e337fc661a0ce2a35f21
Mitzy <rios_mit@yahoo.com>
2023-10-22 16:28:45 -0700

    Carnival Updates

--- index.js
+++ index.js
@@ -12,20 +12,32 @@ let waterIsOn = true
 //Ride 1 works when:
     // green wire is connected
 ride1PowerOn = false
+ if(greenWireConnected){
+    ride1PowerOn= true
+ }
 
 //Ride 2 works when 
     //green and yellow wires are connected
 ride2PowerOn = false
+if(greenWireConnected && yellowWireConnected){
+    ride2PowerOn= true
+}
 
 //Ride 3 works when:
     // green and red wires are connected
     // water is on 
     // yellow wire is not connected
 ride3PowerOn = false
+if(greenWireConnected && redWireConnected && waterIsOn){
+    ride3PowerOn= true
+}
 
 //Ride 4 has power when:
     //ride wire is connected
 ride4PowerOn = false
+if(redWireConnected){
+    ride4PowerOn =true
+}
 //Ride 4 is a slot machine
     //if a 1 is rolled it should return "star"
     //if a 2 is rolled it should return "car"
@@ -35,5 +47,28 @@ ride4PowerOn = false
     //if a 6 is rolled it should return "tiger"
     //if a 7 is rolled it should return "seven"
 function ride4SlotMachine(rolledNumber){
-    return "nothing"
+    switch (rolledNumber){
+            case 1:
+            return"star"
+            break; 
+            case 2:
+                return "car"
+            break; 
+            case 3:
+                return "pot of gold"
+                break; 
+            case 4:
+                return "clover"
+                break; 
+            case 5:
+                return "bowling ball"
+                break; 
+            case 6:
+                return "tiger"
+                break; 
+            case 7:
+                return "seven"
+            default:
+                break;
+    }
 }
